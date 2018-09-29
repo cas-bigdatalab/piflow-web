8cf8d6a080065c2b0c2dc7441216e788d7b00713
sosoll7 <sosoll7@163.com>
2018-09-29 11:15:26 +0800

    Code merge submission
    Code merge submission

--- piflow-web/src/main/java/com/nature/mapper/StopGroupMapper.java
+++ piflow-web/src/main/java/com/nature/mapper/StopGroupMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
+import com.nature.component.workFlow.model.Property;
 import com.nature.component.workFlow.model.StopGroup;
 
 @Mapper
@@ -18,9 +19,10 @@ public interface StopGroupMapper {
 	 */
 	@Select("select * from flow_sotps_groups")
 	List<StopGroup> getStopGroupList();
+
 	@Select("SELECT fsp.id,fsp.name,fsp.description from flow_stops_property fsp where fsp.fk_stops_id = #{id}")
 	public List<Property> getStopGroupList(String id);
 
 	@Update("update flow_stops_property set name = #{name} where id = #{id}")
-	public int updateStops(@Param("name")String content,@Param("id") String id);
+	public int updateStops(@Param("name") String content, @Param("id") String id);
 }
