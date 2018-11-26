147fd1e0e43786ad4a077b6eacd0b1185119c952
sosoll7 <sosoll7@163.com>
2018-11-26 18:36:22 +0800

    Optimization operation

---
+++ piflow-web/src/main/java/com/nature/common/Eunm/ProcessState.java
@@ -0,0 +1,38 @@
+package com.nature.common.Eunm;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.nature.base.TextureEnumSerializer;
+
+@JsonSerialize(using = TextureEnumSerializer.class)
+public enum ProcessState {
+    STARTED("STARTED", "启动"),
+    COMPLETED("COMPLETED", "完成"),
+    FAILED("FAILED", "失败"),
+    ABORTED("ABORTED","中止"),
+    FORK("FORK","FORK");
+
+    private final String value;
+    private final String text;
+
+    private ProcessState(String text, String value) {
+        this.text = text;
+        this.value = value;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static ProcessState selectGender(String name) {
+        for (ProcessState portType : ProcessState.values()) {
+            if (name.equalsIgnoreCase(portType.name())) {
+                return portType;
+            }
+        }
+        return null;
+    }
+}
