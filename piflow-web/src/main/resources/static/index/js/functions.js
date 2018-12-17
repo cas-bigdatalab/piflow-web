5f14cc4f5165846e89c6e1f4abab0721c5cc7128
sosoll7 <sosoll7@163.com>
2018-12-17 11:11:02 +0800

    update indexNew page
    update indexNew page

---
+++ piflow-web/src/main/resources/static/index/js/functions.js
@@ -0,0 +1,79 @@
+$(function() {
+
+
+
+	//===== Hide/show sidebar =====//
+
+	$('.fullview').click(function(){
+	    $("body").toggleClass("clean");
+	    $('#sidebar').toggleClass("hide-sidebar mobile-sidebar");
+	    $('#content').toggleClass("full-content");
+	});
+
+
+
+	//===== Hide/show action tabs =====//
+
+	$('.showmenu').click(function () {
+		$('.actions-wrapper').slideToggle(100);
+	});
+
+
+
+	//===== Easy tabs =====//
+	
+	$('.sidebar-tabs').easytabs({
+		animationSpeed: 150,
+		collapsible: false,
+		tabActiveClass: "active"
+	});
+
+	$('.actions').easytabs({
+		animationSpeed: 300,
+		collapsible: false,
+		tabActiveClass: "current"
+	});
+
+	//===== Collapsible plugin for main nav =====//
+	
+	$('.expand').collapsible({
+		defaultOpen: 'current,third',
+		cookieName: 'navAct',
+		cssOpen: 'subOpened',
+		cssClose: 'subClosed',
+		speed: 200
+	});
+
+		//===== Datatables =====//
+
+		oTable = $('#data-table').dataTable({
+			"bJQueryUI": false,
+			"bAutoWidth": false,
+			"sPaginationType": "full_numbers",
+			"sDom": '<"datatable-header"fl>t<"datatable-footer"ip>',
+			"oLanguage": {
+				"sSearch": "<span>Filter records:</span> _INPUT_",
+				"sLengthMenu": "<span>Show entries:</span> _MENU_",
+				"oPaginate": { "sFirst": "First", "sLast": "Last", "sNext": ">", "sPrevious": "<" }
+			}
+		});
+	
+	
+		oTable = $(".media-table").dataTable({
+			"bJQueryUI": false,
+			"bAutoWidth": false,
+			"sPaginationType": "full_numbers",
+			"sDom": '<"datatable-header"fl>t<"datatable-footer"ip>',
+			"oLanguage": {
+				"sSearch": "<span>Filter records:</span> _INPUT_",
+				"sLengthMenu": "<span>Show entries:</span> _MENU_",
+				"oPaginate": { "sFirst": "First", "sLast": "Last", "sNext": ">", "sPrevious": "<" }
+			},
+			"aoColumnDefs": [
+			  { "bSortable": false, "aTargets": [ 0, 4 ] }
+			]
+		});
+	
+	
+
+});
