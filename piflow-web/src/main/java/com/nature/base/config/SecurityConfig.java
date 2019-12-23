6fb48e49b99282db096a9c6f3f7ce68c6eddbef8
nature <so_nature@163.com>
2019-12-23 16:55:31 +0800

    Guide page problem handling

--- piflow-web/src/main/java/com/nature/base/config/SecurityConfig.java
+++ piflow-web/src/main/java/com/nature/base/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/initPage").permitAll()
+                .antMatchers("/bootPage/initPage").permitAll()
                 .antMatchers("/").permitAll()
                 .antMatchers("/register").permitAll()
                 .antMatchers("/checkUserName").permitAll()
