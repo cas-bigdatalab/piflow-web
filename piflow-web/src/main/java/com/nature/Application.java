1be92eefc175573ff621ec0420f075bf3400ad10
nature <sosoll7@163.com>
2020-01-19 14:37:54 +0800

    Database problem repair

--- piflow-web/src/main/java/com/nature/Application.java
+++ piflow-web/src/main/java/com/nature/Application.java
@@ -11,11 +11,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-
 @PropertySources({
         @PropertySource(value = "classpath:apiConfig.properties", encoding = "utf-8"),
         @PropertySource(value = "classpath:baseConfig.properties", encoding = "utf-8")
@@ -28,107 +23,10 @@ public class Application {
     static Logger logger = LoggerUtil.getLogger();
 
     public static void main(String[] args) {
-        createDatabase();
         ApplicationContext context = SpringApplication.run(Application.class, args);
         SpringContextUtil.setApplicationContext(context);
         logger.warn("***************************************************************");
         logger.warn("***************** Spring Boot Startup Success *****************");
         logger.warn("***************************************************************");
     }
-
-    @SuppressWarnings("resource")
-	public static void createDatabase() {
-        String path = System.getProperty("user.dir") + "/src/main/resources/application.properties";
-        String path_msql = System.getProperty("user.dir") + "/src/main/resources/application-mysql.properties";
-        String driverClassName = null;
-        String username = null;
-        String password = null;
-        String datasourceUrl = null;
-        try {
-            File srcFile = new File(path);
-            if (!srcFile.exists()) {
-                return;
-            }
-            if (!srcFile.isFile()) {
-                return;
-            }
-            BufferedReader br = new BufferedReader(new FileReader(path));
-            String temp = null;
-            Boolean flag = false;
-            while ((temp = br.readLine()) != null) {//readLine()每次读取一行
-                if ("spring.profiles.active=mysql".equals(temp.replaceAll(" ", ""))) {
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) {
-                return;
-            }
-            File srcFile_mysql = new File(path_msql);
-            if (!srcFile_mysql.exists()) {
-                return;
-            }
-            if (!srcFile_mysql.isFile()) {
-                return;
-            }
-            BufferedReader br_mysql = new BufferedReader(new FileReader(path_msql));
-            temp = null;
-            flag = false;
-            while ((temp = br_mysql.readLine()) != null) {//readLine()每次读取一行
-                String temp_trim = temp.replaceAll(" ", "");
-                if (temp_trim.contains("spring.datasource.driver-class-name")) {
-                    String[] split = temp_trim.split("spring.datasource.driver-class-name=");
-                    if (split.length != 2) {
-                        return;
-                    }
-                    driverClassName = split[1];
-                } else if (temp_trim.contains("spring.datasource.url")) {
-                    String[] split = temp_trim.split("spring.datasource.url=");
-                    if (split.length != 2) {
-                        return;
-                    }
-                    datasourceUrl = split[1];
-                } else if (temp_trim.contains("spring.datasource.username")) {
-                    String[] split = temp_trim.split("spring.datasource.username=");
-                    if (split.length != 2) {
-                        return;
-                    }
-                    username = split[1];
-                } else if (temp_trim.contains("spring.datasource.password")) {
-                    String[] split = temp_trim.split("spring.datasource.password=");
-                    if (split.length != 2) {
-                        return;
-                    }
-                    password = split[1];
-                }
-            }
-            if ("com.mysql.jdbc.Driver".equals(driverClassName)) {
-                try {
-                    Class.forName(driverClassName);
-
-                    String url01 = datasourceUrl.substring(0, datasourceUrl.indexOf("?"));
-
-                    String url02 = url01.substring(0, url01.lastIndexOf("/"));
-
-                    String datasourceName = url01.substring(url01.lastIndexOf("/") + 1);
-                    // Connect to existing databases, such as MySQL
-                    Connection connection = DriverManager.getConnection(url02, username, password);
-                    Statement statement = connection.createStatement();
-
-                    // Create a database
-                    statement.executeUpdate("create database if not exists `" + datasourceName + "` default character set utf8 COLLATE utf8_general_ci");
-
-                    statement.close();
-                    connection.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
--- piflow-web/src/main/java/com/nature/base/config/DataSourceConfig.java
+++ piflow-web/src/main/java/com/nature/base/config/DataSourceConfig.java
@@ -1,106 +1,71 @@
-//package com.nature.base.config;
-//
-//
-//import com.alibaba.druid.pool.DruidDataSource;
-//import com.alibaba.druid.support.http.StatViewServlet;
-//import com.alibaba.druid.support.http.WebStatFilter;
-//import com.nature.base.util.LoggerUtil;
-//import org.slf4j.Logger;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.boot.web.servlet.ServletRegistrationBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//
-//import javax.sql.DataSource;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.Statement;
-//
-//
-//@Configuration
-//@Primary //In the same "DataSource", first use the labeled "DataSource"
-//public class DataSourceConfig {
-//
-//    private Logger logger = LoggerUtil.getLogger();
-//
-//    @Value("${spring.datasource.url}")
-//    //jdbc:mysql://127.0.0.1:3306/insight?useUnicode=true&characterEncoding=utf8&failOverReadOnly=false&allowMultiQueries=true
-//    private String datasourceUrl;
-//    @Value("${spring.datasource.driver-class-name}")
-//    private String driverClassName;
-//    @Value("${spring.datasource.username}")
-//    private String username;
-//    @Value("${spring.datasource.password}")
-//    private String password;
-//    @Value("sysParam.whitelist")
-//    private String whitelist;
-//    @Value("sysParam.blacklist")
-//    private String blacklist;
-//    @Value("sysParam.druidUser")
-//    private String druidUser;
-//    @Value("sysParam.druidPassword")
-//    private String druidPassword;
-//
-//    /**
-//     *  Configuration processing of WEB monitoring
-//     */
-//    @Bean
-//    public ServletRegistrationBean druidServlet() {
-//        // Now we're going to do configuration processing for'druid'monitoring
-//        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
-//                new StatViewServlet(), "/druid/*");
-//        // White List, with multiple commas, allows all access if'allow'is not configured or empty
-//        servletRegistrationBean.addInitParameter("allow", whitelist);
-//        // Blacklists, multiple commas (when co-exist,'deny'takes precedence over'allow')
-//        servletRegistrationBean.addInitParameter("deny", blacklist);
-//        // Console Management User Name
-//        servletRegistrationBean.addInitParameter("loginUsername", druidUser);
-//        // Console Management Password
-//        servletRegistrationBean.addInitParameter("loginPassword", druidPassword);
-//        // Can you reset the data source and disable the "Reset All" function on HTML pages?
-//        servletRegistrationBean.addInitParameter("resetEnable", "false");
-//        return servletRegistrationBean;
-//    }
-//
-//    @Bean
-//    public FilterRegistrationBean filterRegistrationBean() {
-//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-//        filterRegistrationBean.setFilter(new WebStatFilter());
-//        //All requests are monitored and processed
-//        filterRegistrationBean.addUrlPatterns("/*");
-//        //Add format information that you don't need to ignore
-//        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.css,/druid/*");
-//        return filterRegistrationBean;
-//    }
-//
-//    @Bean     //Declare it as a bean instance
-//    @ConfigurationProperties(prefix = "spring.datasource")
-//    public DataSource dataSource() {
-//        try {
-//            Class.forName(driverClassName);
-//
-//            String url01 = datasourceUrl.substring(0, datasourceUrl.indexOf("?"));
-//
-//            String url02 = url01.substring(0, url01.lastIndexOf("/"));
-//
-//            String datasourceName = url01.substring(url01.lastIndexOf("/") + 1);
-//            // Connect to existing databases, such as MySQL
-//            Connection connection = DriverManager.getConnection(url02, username, password);
-//            Statement statement = connection.createStatement();
-//
-//            // Create a database
-//            statement.executeUpdate("create database if not exists `" + datasourceName + "` default character set utf8 COLLATE utf8_general_ci");
-//
-//            statement.close();
-//            connection.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        DruidDataSource datasource = new DruidDataSource();
-//        return datasource;
-//    }
-//
-//}
+package com.nature.base.config;
+
+import com.nature.base.util.LoggerUtil;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+
+@Configuration
+@Primary //In the same "DataSource", first use the labeled "DataSource"
+public class DataSourceConfig {
+
+    private Logger logger = LoggerUtil.getLogger();
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrlourceUrl;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.profiles.active}")
+    private String profilesType;
+    
+    @Bean //Declare it as a bean instance
+    public DataSource getDataSource() {
+    	createDatabase();
+    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(datasourceUrlourceUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+    
+    private void createDatabase() {
+    	if("mysql".equals(profilesType)) {
+    		try {
+                Class.forName(driverClassName);
+                //jdbc:mysql://127.0.0.1:3306/insight?useUnicode=true&characterEncoding=utf8&failOverReadOnly=false&allowMultiQueries=true
+                String url01 = datasourceUrlourceUrl.substring(0, datasourceUrlourceUrl.indexOf("?"));
+
+                String url02 = url01.substring(0, url01.lastIndexOf("/"));
+
+                String datasourceName = url01.substring(url01.lastIndexOf("/") + 1);
+                // Connect to existing databases, such as MySQL
+                Connection connection = DriverManager.getConnection(url02, username, password);
+                Statement statement = connection.createStatement();
+
+                // Create a database
+                statement.executeUpdate("create database if not exists `" + datasourceName + "` default character set utf8 COLLATE utf8_general_ci");
+
+                statement.close();
+                connection.close();
+            } catch (Exception e) {
+                logger.error("error",e);
+            }
+    	}
+    }
+}
+
