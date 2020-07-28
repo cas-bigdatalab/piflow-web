c75588779225a1c1978c4c6be10c9345c5e304a7
judy0131 <xiaojie.zhu@163.com>
2020-07-28 04:50:54 -0400

    add StopsHub Mapper related API

---
+++ piflow-web/src/test/java/cn/cnic/mapper/StopsHubMapperTest.java
@@ -0,0 +1,73 @@
+package cn.cnic.mapper;
+
+import cn.cnic.ApplicationTests;
+import cn.cnic.base.util.LoggerUtil;
+import cn.cnic.base.util.UUIDUtils;
+import cn.cnic.common.Eunm.StopsHubState;
+import cn.cnic.component.stopsComponent.model.StopsHub;
+import cn.cnic.mapper.stopsComponent.StopsHubMapper;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+public class StopsHubMapperTest extends ApplicationTests {
+
+    @Resource
+    private StopsHubMapper stopsHubMapper;
+
+    Logger logger = LoggerUtil.getLogger();
+
+    @Test
+    public void testAddStopsHub() {
+
+        StopsHub stopsHub = new StopsHub();
+        stopsHub.setId(UUIDUtils.getUUID32());
+        stopsHub.setCrtUser("Nature");
+        stopsHub.setCrtDttm(new Date());
+        stopsHub.setLastUpdateUser("Nature");
+        stopsHub.setLastUpdateDttm(new Date());
+        stopsHub.setEnableFlag(true);
+        stopsHub.setVersion(1L);
+        stopsHub.setJarName("piflowexternal1.jar");
+        stopsHub.setJarUrl("/data/piflowServer/class/piflowexternal1.jar");
+        stopsHub.setStatus(StopsHubState.MOUNT);
+        int result = stopsHubMapper.addStopHub(stopsHub);
+        if (1 == result) {
+            logger.info("The query result is empty");
+        }
+        logger.info(stopsHub.toString());
+    }
+
+    @Test
+    public void testUpdateStopsHub() {
+
+        StopsHub stopsHub = new StopsHub();
+        stopsHub.setId("d6850e11266b42c9bdeab7595921a647");
+        stopsHub.setCrtUser("Nature");
+        stopsHub.setCrtDttm(new Date());
+        stopsHub.setLastUpdateUser("Nature");
+        stopsHub.setLastUpdateDttm(new Date());
+        stopsHub.setEnableFlag(true);
+        stopsHub.setVersion(1L);
+        stopsHub.setJarName("piflowexternal1.jar");
+        stopsHub.setJarUrl("/data/piflowServer/class/piflowexternal1.jar");
+        stopsHub.setStatus(StopsHubState.UNMOUNT);
+        int result = stopsHubMapper.updateStopHub(stopsHub);
+        if (1 == result) {
+            logger.info("The query result is empty");
+        }
+        logger.info(stopsHub.toString());
+    }
+
+    @Test
+    public void testGetAllStopsHub() {
+        List<StopsHub> result = stopsHubMapper.getStopsHubList("Nature",false);
+        for(int i=0; i<result.size(); i++){
+            StopsHub s = result.get(i);
+            System.out.println(s.toString());
+        }
+    }
+}
