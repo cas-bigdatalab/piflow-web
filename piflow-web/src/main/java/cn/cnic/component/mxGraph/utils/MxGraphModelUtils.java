45901206d0d8e855914df62805cb1381844837e8
nature <sosoll7@163.com>
2020-09-03 22:20:16 +0800

    web schedule api

--- piflow-web/src/main/java/cn/cnic/component/mxGraph/utils/MxGraphModelUtils.java
+++ piflow-web/src/main/java/cn/cnic/component/mxGraph/utils/MxGraphModelUtils.java
@@ -37,7 +37,7 @@ public class MxGraphModelUtils {
             if (isAddId) {
                 mxGraphModel.setId(UUIDUtils.getUUID32());
             }
-            return mxGraphModelNewNoId(username);
+            return mxGraphModel;
         }
         // basic properties (required when creating)
         mxGraphModel.setCrtDttm(new Date());
