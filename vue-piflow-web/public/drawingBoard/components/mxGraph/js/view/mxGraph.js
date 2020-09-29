ce7d8aa2b04c1df79091bafeb0e02483962baed5
nature <sosoll7@163.com>
2020-09-29 14:18:56 +0800

    repair mxGraph img bug

--- vue-piflow-web/public/drawingBoard/components/mxGraph/js/view/mxGraph.js
+++ vue-piflow-web/public/drawingBoard/components/mxGraph/js/view/mxGraph.js
@@ -2470,6 +2470,9 @@ mxGraph.prototype.getEditingValue = function(cell, evt)
 mxGraph.prototype.stopEditing = function(cancel)
 {
 	this.cellEditor.stopEditing(cancel);
+//------------------------------ Custom modification content 002 start ------------------------------
+    EditorUi.prototype.saveGraphData(null, 'MOVED');
+//------------------------------ Custom modification content 002 end ------------------------------
 	this.fireEvent(new mxEventObject(mxEvent.EDITING_STOPPED, 'cancel', cancel));
 };
 
