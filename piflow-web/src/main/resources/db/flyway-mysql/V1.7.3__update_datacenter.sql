c064f5fba43348ea5f9a54f55f457bda5c262e14
nature <sosoll7@163.com>
2022-05-26 09:50:57 +0800

    update sql script

--- piflow-web/src/main/resources/db/flyway-mysql/V1.7.3__update_datacenter.sql
+++ piflow-web/src/main/resources/db/flyway-mysql/V1.7.3__update_datacenter.sql
@@ -1,30 +1,8 @@
 -- ----------------------------
--- Table flow_stops_template,Add column
+-- Add column
 -- ----------------------------
-ALTER TABLE `flow_stops_template`
-ADD COLUMN `is_data_source` bit(1) NULL AFTER `visualization_type`;
-
-
--- ----------------------------
--- Table flow_stops,Add column
--- ----------------------------
-ALTER TABLE `flow_stops`
-ADD COLUMN `is_data_source` bit(1) NULL AFTER `fk_data_source_id`;
-
-
-
--- ----------------------------
--- Table flow_process_stop,Add column
--- ----------------------------
-ALTER TABLE `flow_process_stop`
-ADD COLUMN `is_data_source` bit(1) NULL AFTER `fk_flow_process_id`;
-
-
--- ----------------------------
--- Table data_source,Add foreign key and column
--- ----------------------------
-ALTER TABLE `flow_stops_template`ADD INDEX(`bundel`);
-
-ALTER TABLE `data_source`
-ADD COLUMN `stops_template_bundle` varchar(255) NULL AFTER `is_template`,
-ADD CONSTRAINT `FKqwv1iytgkhhgnjdvhqbsknas6` FOREIGN KEY (`stops_template_bundle`) REFERENCES `flow_stops_template` (`bundel`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `flow_stops_template` ADD COLUMN `is_data_source` bit(1) NULL AFTER `visualization_type`;
+ALTER TABLE `flow_stops_template` ADD INDEX(`bundel`);
+ALTER TABLE `flow_stops` ADD COLUMN `is_data_source` bit(1) NULL AFTER `fk_data_source_id`;
+ALTER TABLE `flow_process_stop` ADD COLUMN `is_data_source` bit(1) NULL AFTER `fk_flow_process_id`;
+ALTER TABLE `data_source` ADD COLUMN `stops_template_bundle` varchar(255) NULL AFTER `is_template`;
