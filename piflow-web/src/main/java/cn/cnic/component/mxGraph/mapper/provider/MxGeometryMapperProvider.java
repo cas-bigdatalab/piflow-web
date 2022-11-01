2abce99ec807cbbd14c9d76ffa0f3d96b80e5b25
sosoll7 <sosoll7@163.com>
2022-11-01 15:49:04 +0800

    repair delete flow

--- piflow-web/src/main/java/cn/cnic/component/mxGraph/mapper/provider/MxGeometryMapperProvider.java
+++ piflow-web/src/main/java/cn/cnic/component/mxGraph/mapper/provider/MxGeometryMapperProvider.java
@@ -200,7 +200,7 @@ public class MxGeometryMapperProvider {
             return "SELECT 0";
         }
         SQL sql = new SQL();
-        sql.UPDATE("mx_graph_model");
+        sql.UPDATE("mx_geometry");
         sql.SET("enable_flag = 0");
         sql.SET("last_update_user = " + SqlUtils.preventSQLInjection(username));
         sql.SET("last_update_dttm = " + SqlUtils.preventSQLInjection(DateUtils.dateTimesToStr(new Date())));
