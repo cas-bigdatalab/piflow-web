2cea3c9c693bd522d87ff04ae64f8785d5737a21
tianyao <tianyao_0315@163.com>
2023-09-04 11:25:15 +0800

    #schedule_python

--- piflow-web/src/main/java/cn/cnic/third/service/impl/ScheduleImpl.java
+++ piflow-web/src/main/java/cn/cnic/third/service/impl/ScheduleImpl.java
@@ -1,10 +1,17 @@
 package cn.cnic.third.service.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import cn.cnic.common.Eunm.ComponentFileType;
 import cn.cnic.common.constant.ApiConfig;
 import cn.cnic.common.constant.MessageConfig;
+import cn.cnic.component.process.entity.ProcessGroupPath;
+import cn.cnic.component.process.entity.ProcessStop;
+import cn.cnic.component.stopsComponent.domain.StopsComponentDomain;
+import cn.cnic.component.stopsComponent.entity.StopsComponent;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
@@ -31,18 +38,43 @@ public class ScheduleImpl implements ISchedule {
      * Introducing logs, note that they are all packaged under "org.slf4j"
      */
     private Logger logger = LoggerUtil.getLogger();
+    private final StopsComponentDomain stopsComponentDomain;
+
+    public ScheduleImpl(StopsComponentDomain stopsComponentDomain) {
+        this.stopsComponentDomain = stopsComponentDomain;
+    }
 
     @Override
     public Map<String, Object> scheduleStart(Schedule schedule, Process process, ProcessGroup processGroup) {
         if (null == schedule) {
             return ReturnMapUtils.setFailedMsg("failed, schedule is null");
         }
+        if (null == process) {
+            return ReturnMapUtils.setFailedMsg(MessageConfig.PARAM_ERROR_MSG());
+        }
+        //String json = ProcessUtil.processToJson(process, checkpoint, runModeType);
+        //String formatJson = JsonFormatTool.formatJson(json);
+        List<ProcessStop> processStopList = process.getProcessStopList();
+        if (processStopList == null || processStopList.size() == 0) {
+            return ReturnMapUtils.setFailedMsg(MessageConfig.PARAM_IS_NULL_MSG("Stop"));
+        }else {
+            for (ProcessStop processStop : processStopList) {
+                StopsComponent stops = stopsComponentDomain.getOnlyStopsComponentByBundle(processStop.getBundel());
+                if (stops == null) {
+                    return ReturnMapUtils.setFailedMsg(MessageConfig.DATA_ERROR_MSG());
+                }
+                processStop.setComponentType(stops.getComponentType());
+                if (ComponentFileType.PYTHON == processStop.getComponentType()) {
+                    processStop.setDockerImagesName(stops.getDockerImagesName());
+                }
+            }
+        }
         String type = schedule.getType();
         Map<String, Object> scheduleContentMap = new HashMap<>();
         if ("FLOW".equals(type) && null != process) {
             scheduleContentMap = ProcessUtils.processToMap(process, "", RunModeType.RUN, process.getFlowGlobalParamsList());
         } else if ("FLOW_GROUP".equals(type) && null != processGroup) {
-            scheduleContentMap = ProcessUtils.processGroupToMap(processGroup, RunModeType.RUN);
+            scheduleContentMap = this.processGroupToMap(processGroup, RunModeType.RUN);
         } else {
             return ReturnMapUtils.setFailedMsg(MessageConfig.SCHEDULED_TYPE_OR_DATA_ERROR_MSG());
         }
@@ -67,6 +99,87 @@ public class ScheduleImpl implements ISchedule {
             return ReturnMapUtils.setFailedMsg("Error : " + MessageConfig.INTERFACE_CALL_ERROR_MSG());
         }
         return ReturnMapUtils.setSucceededCustomParam("scheduleId", sendPostData);
+    }
+
+    public Map<String, Object> processGroupToMap(ProcessGroup processGroup, RunModeType runModeType) {
+
+        Map<String, Object> rtnMap = new HashMap<>();
+        Map<String, Object> flowGroupVoMap = new HashMap<>();
+        flowGroupVoMap.put("name", processGroup.getName());
+        flowGroupVoMap.put("uuid", processGroup.getId());
+
+        // all process
+        Map<String, Process> processesMap = new HashMap<>();
+        Map<String, ProcessGroup> processGroupsMap = new HashMap<>();
+
+        List<Process> processList = processGroup.getProcessList();
+        if (null != processList && processList.size() > 0) {
+            List<Map<String, Object>> processesListMap = new ArrayList<>();
+            for (Process process : processList) {
+
+                List<ProcessStop> processStopList = process.getProcessStopList();
+                if (processStopList == null || processStopList.size() == 0) {
+                    return ReturnMapUtils.setFailedMsg(MessageConfig.PARAM_IS_NULL_MSG("Stop"));
+                }else {
+                    for (ProcessStop processStop : processStopList) {
+                        StopsComponent stops = stopsComponentDomain.getOnlyStopsComponentByBundle(processStop.getBundel());
+                        if (stops == null) {
+                            return ReturnMapUtils.setFailedMsg(MessageConfig.DATA_ERROR_MSG());
+                        }
+                        processStop.setComponentType(stops.getComponentType());
+                        if (ComponentFileType.PYTHON == processStop.getComponentType()) {
+                            processStop.setDockerImagesName(stops.getDockerImagesName());
+                        }
+                    }
+                }
+                processesMap.put(process.getPageId(), process);
+                Map<String, Object> processMap = ProcessUtils.processToMap(process, null, runModeType, process.getFlowGlobalParamsList());
+                processesListMap.add(processMap);
+            }
+            flowGroupVoMap.put("flows", processesListMap);
+        }
+
+        List<ProcessGroup> processGroupList = processGroup.getProcessGroupList();
+        if (null != processGroupList && processGroupList.size() > 0) {
+            List<Map<String, Object>> processesGroupListMap = new ArrayList<>();
+            for (ProcessGroup processGroupNew : processGroupList) {
+                processGroupsMap.put(processGroupNew.getPageId(), processGroupNew);
+                Map<String, Object> processGroupMap = processGroupToMap(processGroupNew, runModeType);
+                processesGroupListMap.add(processGroupMap);
+            }
+            flowGroupVoMap.put("groups", processesGroupListMap);
+
+        }
+
+        List<ProcessGroupPath> processGroupPathList = processGroup.getProcessGroupPathList();
+        if (null != processGroupPathList && processGroupPathList.size() > 0) {
+            List<Map<String, Object>> pathListMap = new ArrayList<>();
+            for (ProcessGroupPath processGroupPath : processGroupPathList) {
+                if (null != processGroupPath) {
+                    Map<String, Object> pathMap = new HashMap<>();
+                    String formName = "";
+                    String toName = "";
+                    String from = processGroupPath.getFrom();
+                    String to = processGroupPath.getTo();
+                    if (null != processesMap.get(from)) {
+                        formName = processesMap.get(from).getName();
+                    } else if (null != processGroupsMap.get(from)) {
+                        formName = processGroupsMap.get(from).getName();
+                    }
+                    if (null != processesMap.get(to)) {
+                        toName = processesMap.get(to).getName();
+                    } else if (null != processGroupsMap.get(to)) {
+                        toName = processGroupsMap.get(to).getName();
+                    }
+                    pathMap.put("after", formName);
+                    pathMap.put("entry", toName);
+                    pathListMap.add(pathMap);
+                }
+            }
+            flowGroupVoMap.put("conditions", pathListMap);
+        }
+        rtnMap.put("group", flowGroupVoMap);
+        return rtnMap;
     }
 
     @Override
