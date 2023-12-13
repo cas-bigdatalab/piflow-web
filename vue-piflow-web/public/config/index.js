4d371f896a889ff826371ee6da499f5668cd5480
tianyao-0315 <tianyao_0315@163.com>
2023-12-13 13:52:04 +0800

    #首页界面可配置

---
+++ vue-piflow-web/public/config/index.js
@@ -0,0 +1,13 @@
+// 标题
+window.$SYSTEM_TITLE_EN = 'πFlow Big Data Pipeline System'
+window.$SYSTEM_TITLE_ZH = "πFlow大数据流水线管理系统"
+
+// 介绍
+window.$SYSTEM_INTRODUCTION_INFO_EN = "πFlow is a big data pipeline system developed based on the distributed computing engine Spark. It realizes the flow configuration, operation and intelligent monitoring of big data collection, processing, storage and analysis in a WYSIWYG way, providing 100+ standardization Components, including Hadoop, Spark, MLlib, Hive, Solr, ElasticSearch, Redis, etc., supporting field-oriented and flexible secondary component development with superior performance."
+window.$SYSTEM_INTRODUCTION_INFO_ZH = "πFlow是一款基于分布式计算引擎Spark开发的大数据流水线系统，以所见即所得的方式，实现大数据采集、处理、存储与分析流程化配置、运行与智能监控，提供了100+的标准化组件， 包括Hadoop 、Spark、MLlib、Hive、Solr、ElasticSearch、Redis等，更支持面向领域、灵活的二次组件开发，性能优越。"
+
+
+
+
+
+document.title = window.$SYSTEM_TITLE_EN 
