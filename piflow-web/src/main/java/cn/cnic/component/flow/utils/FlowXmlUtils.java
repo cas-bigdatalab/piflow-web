ec85a7b56780d2a629eda7b4ea7ec1f7e7b1b56b
zhouziang <zhouziang1996@outlook.com>
2024-03-29 14:22:14 +0800

    fix:修复模板功能遗留问题

--- piflow-web/src/main/java/cn/cnic/component/flow/utils/FlowXmlUtils.java
+++ piflow-web/src/main/java/cn/cnic/component/flow/utils/FlowXmlUtils.java
@@ -258,8 +258,6 @@ public class FlowXmlUtils {
                         xmlStrSb.append(spliceStr("required", required));
                         xmlStrSb.append(spliceStr("sensitive", sensitive));
                         xmlStrSb.append(spliceStr("isSelect", isSelect));
-                        //python算子保存参数顺序,load时顺序要保持
-                        xmlStrSb.append(spliceStr("propertySort", propertySort));
                         xmlStrSb.append("/> \n");
                     }
                 }
