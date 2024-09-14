a63e5da34e14b5204c0ca1426ad435931a1737a1
zk <1350534586@qq.com>
2024-09-14 10:03:13 +0800

    可视化vuex 迁移

--- vue-piflow-web/src/store/index.js
+++ vue-piflow-web/src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import variable from '../modules/variable'
+import graphConf from './graphConf'
 
 Vue.use(Vuex)
 const store = new Vuex.Store({
   modules: {
     variable,
+    graphConf
   }
 });
 export default store;
