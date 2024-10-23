907b6c67604efc4332fffef57b5d45b7c2809252
zhouziang <zhouziang1996@outlook.com>
2024-10-23 21:14:47 +0800

    修改请求参数

--- piflow-web/src/main/java/cn/cnic/controller/requestVo/GetProcessHistoryRequestVo.java
+++ piflow-web/src/main/java/cn/cnic/controller/requestVo/GetProcessHistoryRequestVo.java
@@ -9,7 +9,7 @@ public class GetProcessHistoryRequestVo {
     private Integer limit;
     private Integer page;
     private String keyword;//用于接收关键词查询的关键词
-    private String name;
+    private String createUser;
     private String state;
 
 
